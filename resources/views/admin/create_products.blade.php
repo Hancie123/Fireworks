4c8ba9c0336d13d5fe96fe07f4b482aeacb794df
Hancie Phago <Hanciewanemphago@gmail.com>
2023-03-26 17:39:33 +0545

    commit change

--- resources/views/admin/create_products.blade.php
+++ resources/views/admin/create_products.blade.php
@@ -6,6 +6,14 @@
 <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
 <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css" />
+
+
+<link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.7.1/css/buttons.dataTables.min.css">
+<script src="https://cdn.datatables.net/buttons/1.7.1/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.7.1/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.7.1/js/buttons.print.min.js"></script>
+
+
 <div class="main-panel">
     <div class="content-wrapper">
         <h3><b>Create Products</b></h3><br>
@@ -121,10 +129,19 @@
                     {
                         data: 'date'
                     }
+                ],
+                dom: 'Bfrtip',
+                buttons: [
+                    'copyHtml5',
+                    'excelHtml5',
+                    'csvHtml5',
+                    'pdfHtml5',
+                    'print'
                 ]
             });
         });
         </script>
+
 
         <style>
         /* Change the background color of the table header */
